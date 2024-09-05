d953ddca39c7e469281fc588b3719aeff049cd7d
cr4ftily <kravtilya@gmail.com>
2024-09-05 15:43:39 -0500

    enums for cards

---
+++ card_enums.py
@@ -0,0 +1,100 @@
+from enum import Enum
+
+class Cards(Enum):
+    new = "Newcomer"
+    mua = "Make-Up Artist"
+    mau = "Make-Up Artist"
+    mak = "Make-Up Artist"
+    ms = "Movie Star"
+    mov = "Movie Star"
+    gan = "Gangster"
+    clo = "Clown" # clones
+    ven = "Vendor"
+    jug = "Juggler"
+    pon = "Pony"
+    but = "Butler"
+    ske = "Skeleton"
+    spi = "Spider"
+    rp = "Rescue Pod"
+    res = "Rescue Pod"
+    ss = "Shapeshifter"
+    sha = "Shapeshifter" # shark
+    ai = "A.I."
+    art = "A.I."
+    jes = "Jester"
+    sb = "Stable Boy"
+    sta = "Stable Boy"
+    her = "Hermit" # herione
+    sai = "Sailor"
+    tre = "Treasure" # t rex
+    par = "Parrot"
+    rep = "Reporter"
+    tal = "Talent"
+    ban = "Band"
+    ali = "Alien"
+    gho = "Ghost"
+    tee = "Teenager"
+    teen = "Teenager"
+    nec = "Necromancer"
+    necro = "Necromancer"
+    mim = "Mime"
+    pyr = "Pyrotechnician"
+    cla = "Clairvoyant"
+    rd = "Rubber Duck"
+    rub = "Rubber Duck"
+    cb = "Cowboy"
+    cc = "Comic Character"
+    com = "Comic Character"
+    dir = "Director"
+    lio = "Lion"
+    bla = "Blacksmith"
+    kni = "Knight"
+    wiz = "Wizard"
+    hor = "Horse"
+    coo = "Cook"
+    nav = "Navigator"
+    lif = "Lifegaurd"
+    mas = "Mascot"
+    hol = "Hologram"
+    holo = "Hologram"
+    sfg = "Sci-Fi Geek"
+    sg = "Sci-Fi Geek"
+    sci = "Scie-Fi Geek"
+    sli = "Slime"
+    vam = "Vampire"
+    vc = "Vacuum Cleaner"
+    vac = "Vacuum Cleaner"
+    wer = "Werewolf"
+    ill = "Illusionist"
+    magician = "Illusionist"
+    mag = "Illusionist"
+    bc = "Bumber Car"
+    bum = "Bumper Car"
+    tb = "Teddy Bear"
+    ted = "Teddy Bear"
+    rex = "T-rex"
+    vil = "Villian"
+    vilian = "Villian"
+    bar = "Bard"
+    pri = "Prince"
+    dra = "Dragon"
+    sir = "Siren"
+    kra = "Kraken"
+    sub = "Submarine"
+    cha = "Champion"
+    champ = "Champion"
+    fb = "Fan-Bus"
+    fan = "Fan-Bus"
+
+# # Access enum members
+# print(Color.RED)            # Output: Color.RED
+# print(Color.GREEN.name)     # Output: 'GREEN'
+# print(Color.BLUE.value)     # Output: 3
+
+# # Compare enum members
+# if Color.RED == Color(1):
+#     print("The color is RED")  # This will be printed
+
+# # Looping through enum members
+# for color in Color:
+#     print(color)
